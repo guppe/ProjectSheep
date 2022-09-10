963965daa7ec854d1ee38ed0baefd5f81aeb2c89
guppe <mst39639toi8@gmail.com>
2022-09-11 00:00:33 +0900

    navigaiton drawer OK

--- app/src/main/java/com/websarva/wings/android/projectsheep/TimeTableActivity.kt
+++ app/src/main/java/com/websarva/wings/android/projectsheep/TimeTableActivity.kt
@@ -2,6 +2,7 @@ package com.websarva.wings.android.projectsheep
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -21,6 +22,8 @@ class TimeTableActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time_table)
+
+
 
         val viewPager2: ViewPager2 = findViewById(R.id.viewPager2)
         val tabLayout: TabLayout = findViewById(R.id.tabLayout)
