f1b78a7e18d82e4ccddafb94828a69a6a4d6de4f
guppe <mst39639toi8@gmail.com>
2022-09-11 10:08:40 +0900

    toolbar changed material toolbar

--- app/src/main/java/com/websarva/wings/android/projectsheep/MapViewActivity.kt
+++ app/src/main/java/com/websarva/wings/android/projectsheep/MapViewActivity.kt
@@ -1,11 +1,28 @@
+
 package com.websarva.wings.android.projectsheep
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class MapViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map_view)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var returnVal = true
+
+        if(item.itemId == android.R.id.home) {
+            finish()
+        }
+
+        else {
+            returnVal = super.onOptionsItemSelected(item)
+        }
+        return returnVal
     }
 }
--- app/src/main/java/com/websarva/wings/android/projectsheep/MondayFragment.kt
+++ app/src/main/java/com/websarva/wings/android/projectsheep/MondayFragment.kt
@@ -1,10 +1,12 @@
 package com.websarva.wings.android.projectsheep
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_monday.*
 
 class MondayFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +14,7 @@ class MondayFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_monday, container, false)
     }
 }
